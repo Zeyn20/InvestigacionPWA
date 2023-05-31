ab84c4dbec3bde75b90f1d60c07e88d57d2f3221
Eyner Vergara <67844943+Zeyn20@users.noreply.github.com>
2023-05-31 10:07:20 -0500

    prueba

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@ const showCoffees = () => {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
       navigator.serviceWorker
-        .register("/serviceWorker.js")
+        .register("SW/serviceWorker.js")
         .then(res => console.log("service worker registered"))
         .catch(err => console.log("service worker not registered", err))
     })
