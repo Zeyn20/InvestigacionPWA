8b2ab85938cf7d4835bafdbcffbea8c3b16c5249
Eyner Vergara <67844943+Zeyn20@users.noreply.github.com>
2023-05-31 10:35:18 -0500

    prueba

--- SW/serviceWorker.js
+++ SW/serviceWorker.js
@@ -17,15 +17,26 @@ const assets = [
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
     caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets)
+      return cache.addAll(assets);
     })
-  )
-})
+  );
+});
+
+self.addEventListener("activate", activateEvent => {
+  activateEvent.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => cacheName !== staticDevCoffee)
+                  .map(cacheName => caches.delete(cacheName))
+      );
+    })
+  );
+});
 
 self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-      caches.match(fetchEvent.request).then(res => {
-        return res || fetch(fetchEvent.request)
-      })
-    )
-  })
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(cachedResponse => {
+      return cachedResponse || fetch(fetchEvent.request);
+    })
+  );
+});
