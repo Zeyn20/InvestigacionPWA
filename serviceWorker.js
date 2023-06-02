7e1cc4c216e57c96eb353d63b4b944baca678582
Eyner Vergara <67844943+Zeyn20@users.noreply.github.com>
2023-06-02 17:47:52 -0500

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -13,7 +13,7 @@ const assets = [
   "/images/coffee7.jpg",
   "/images/coffee8.jpg",
   "/images/coffee9.jpg",
-]
+];
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
